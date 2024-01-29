5ccd06350c34a647706cf6d88a89ebfadadc6226
Shivam <mishrashiv1708@gmail.com>
2024-01-29 16:00:34 +0530

    Prompt , Alert & confirm

---
+++ practice6.js
@@ -0,0 +1,18 @@
+/* Q1 : Write a program using prompt funnction to take input from user of age as a value from tthe userr and add alert to tell him if he can drive ! 
+*/
+
+let age = prompt("enter your age");
+age = Number.parseInt(age);
+const canDrive = (age)=>{}
+if (canDrive(age)){
+    alert("YOu can drive");
+    }
+else {
+    alert('You can not drive');
+}
+// if (age >= 18){
+//     alert("YOu can drive");
+// }
+// else {
+//     alert('You can not drive');
+// } 
